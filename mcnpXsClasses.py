58b28d4bd4607ab1a1bc46f76e1862a72cebfe84
eNorris <812xsi@gmail.com>
2014-04-13 15:49:28 -0500

    Added isotope data and the start to the mcnp material classes

---
+++ mcnpXsClasses.py
@@ -0,0 +1,23 @@
+
+class CrossSection:
+
+	def __init__(self):
+		self.energy = [];
+		self.value = [];
+		
+class Isotope:
+
+	def __init__(self):
+		self.zaid = 00000;
+		pass;
+		
+class Element:
+
+	def __init__(self):
+		pass;
+		
+class Material:
+
+	def __init__(self):
+		pass;
+		
