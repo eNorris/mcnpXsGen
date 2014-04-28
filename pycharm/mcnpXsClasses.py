e483a4903f30836e2c11e75135cdc9d6bf8bbf6d
eNorris <812xsi@gmail.com>
2014-04-27 21:42:10 -0500

    updated xsgen format

--- pycharm/mcnpXsClasses.py
+++ pycharm/mcnpXsClasses.py
@@ -11,25 +11,28 @@
 #    MF=12-15 contain photon production data, and
 #    MF=30-36 contain covariance data.
 
+
 class CrossSection:
 
-	def __init__(self):
-		self.energy = [];
-		self.value = [];
-		
+    def __init__(self):
+        self.energy = []
+        self.value = []
+        
+
 class Isotope:
 
-	def __init__(self):
-		self.zaid = 00000;
-		pass;
-		
+    def __init__(self):
+        self.zaid = 00000
+        pass
+
+
 class Element:
 
-	def __init__(self):
-		pass;
-		
+    def __init__(self):
+        pass
+
+
 class Material:
 
-	def __init__(self):
-		pass;
-		
+    def __init__(self):
+        pass
